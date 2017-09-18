7dda3c47a2c97ff3c1f83883ae42e7d0fb60c6bb
linghuam <1551827898@qq.com>
2017-09-18 18:02:36 +0800

    c
    Signed-off-by: linghuam <1551827898@qq.com>

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,16 @@
-alert('aaaannbbbb');
+// import L from 'leaflet'
+// import 'leaflet/dist/leaflet.css'
+// import { GoogleLayer } from './leaflet.googlelayer'
+
+// var map = L.map('map', {
+//   minZoom: 2,
+//   maxZoom: 18,
+//   zoom: 4,
+//   crs: L.CRS.EPSG3857,
+//   center: [24, 106],
+//   trackResize: true
+// });
+
+// new GoogleLayer('http://mt{num}.google.cn/vt/lyrs=m&hl=zh-CN&gl=cn&s=Gal&z={z}&x={x}&y={y}').addTo(map);
+
+alert('aa');
