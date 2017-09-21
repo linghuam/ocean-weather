602e12761223c4adc436be5cdd7a1af37a71dd92
linghuam <1551827898@qq.com>
2017-09-21 23:25:12 +0800

    stop

--- src/components/menu.vue
+++ src/components/menu.vue
@@ -22,35 +22,37 @@ export default {
 
   methods: {
     btnClickEvt(type) {
+
+      this._funcWind && this._funcWind.stop();
+      this._funcWind2 && this._funcWind2.stop();
+      this._funcFlow && this._funcFlow.stop();
+      this._funcPressure && this._funcPressure.stop();
+      this._funcVisibility && this._funcVisibility.stop();
+
       if(type === '风') {
         if(!this._funcWind) {
           this._funcWind = new FuncWind(this.$parent.$data.map);
         }
-        this._funcWind.stop();
         this._funcWind.start();
       } else if(type === '风2') {
         if(!this._funcWind2) {
           this._funcWind2 = new FuncWindMe(this.$parent.$data.map);
         }
-        this._funcWind2.stop();
         this._funcWind2.start();
       } else if(type === '洋流') {
         if(!this._funcFlow) {
           this._funcFlow = new FuncFlow(this.$parent.$data.map);
         }
-        this._funcFlow.stop();
         this._funcFlow.start();
       } else if(type === '气压') {
         if(!this._funcPressure) {
           this._funcPressure = new FuncPressure(this.$parent.$data.map);
         }
-        this._funcPressure.stop();
         this._funcPressure.start();
       } else if(type === '能见度') {
         if(!this._funcVisibility) {
           this._funcVisibility = new FuncVisibility(this.$parent.$data.map);
         }
-        this._funcVisibility.stop();
         this._funcVisibility.start();
       }
     }
