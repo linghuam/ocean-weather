0f4b566f06f42010b1183e1b243464f88a125ec8
linghuam <1551827898@qq.com>
2017-10-13 15:20:04 +0800

    textpath

--- src/js/func.pressure.js
+++ src/js/func.pressure.js
@@ -28,58 +28,58 @@ export class FuncPressure {
     Papa.parse('./static/data/pressure.csv', {
       download: true,
       complete: function (results) {
-        var r = results;
+        // var r = results;
       },
-      // step: function (results, parser) {
-      //   if(results.data[0][0] === '' && arr.length) {
+      step: function (results, parser) {
+        if(results.data[0][0] === '' && arr.length) {
 
-      //     // handler data -------------start------------------------
-      //     var latlngs = [];
-      //     for(var i = 0, len = arr.length; i < len; i++) {
-      //       var row = arr[i];
-      //       var lat = +row[0];
-      //       var lng = +row[1];
-      //       var value = options.text = +row[2];
-      //       var latlng = L.latLng(lat, lng);
+          // handler data -------------start------------------------
+          var latlngs = [];
+          for(var i = 0, len = arr.length; i < len; i++) {
+            var row = arr[i];
+            var lat = +row[0];
+            var lng = +row[1];
+            var value = options.text = +row[2];
+            var latlng = L.latLng(lat, lng);
 
-      //       if(i === 0) {
-      //         latlngs.push(latlng);
-      //       } else {
-      //         var lastlng = (latlngs[latlngs.length - 1]).lng;
-      //         var extend = Math.abs(lng - lastlng);
-      //         if(extend >= 180) { //解决经度范围超过180连线异常
-      //           if(this._map.hasLayer(this.pressureFeatureGroup) && latlngs.length >=2) {
-      //             var geo = L.polyline(latlngs, options).toGeoJSON();
-      //             var curved = turf.bezier(geo, 10000, 1);
-      //             var newlatlngs = [];
-      //             for (let i =0, len = curved.geometry.coordinates.length; i < len; i++){
-      //               newlatlngs.push(L.latLng(curved.geometry.coordinates[i][1],curved.geometry.coordinates[i][0]));
-      //             }
-      //             this.pressureFeatureGroup.addLayer(L.polyline(newlatlngs, options));
-      //           }
-      //           latlngs = [];
-      //           latlngs.push(latlng);
-      //         } else {
-      //           latlngs.push(latlng);
-      //           if(i === len - 1) {
-      //             if(this._map.hasLayer(this.pressureFeatureGroup)) {
-      //               this.pressureFeatureGroup.addLayer(L.polyline(latlngs, options));
-      //             }
-      //             latlngs = [];
-      //           }
-      //         }
-      //       }
-      //     }
-      //     // handler datq ----------------end-----------------------------
-      //     arr = [];
-      //   } else {
-      //     if(arr) {
-      //       arr.push(results.data[0]);
-      //     } else {
-      //       arr = [];
-      //     }
-      //   }
-      // }.bind(this)
+            if(i === 0) {
+              latlngs.push(latlng);
+            } else {
+              var lastlng = (latlngs[latlngs.length - 1]).lng;
+              var extend = Math.abs(lng - lastlng);
+              if(extend >= 180) { //解决经度范围超过180连线异常
+                if(this._map.hasLayer(this.pressureFeatureGroup) && latlngs.length >=2) {
+                  var geo = L.polyline(latlngs, options).toGeoJSON();
+                  var curved = turf.bezier(geo, 10000, 1);
+                  var newlatlngs = [];
+                  for (let i =0, len = curved.geometry.coordinates.length; i < len; i++){
+                    newlatlngs.push(L.latLng(curved.geometry.coordinates[i][1],curved.geometry.coordinates[i][0]));
+                  }
+                  this.pressureFeatureGroup.addLayer(L.polyline(latlngs, options));
+                }
+                latlngs = [];
+                latlngs.push(latlng);
+              } else {
+                latlngs.push(latlng);
+                if(i === len - 1) {
+                  if(this._map.hasLayer(this.pressureFeatureGroup)) {
+                    this.pressureFeatureGroup.addLayer(L.polyline(latlngs, options));
+                  }
+                  latlngs = [];
+                }
+              }
+            }
+          }
+          // handler datq ----------------end-----------------------------
+          arr = [];
+        } else {
+          if(arr) {
+            arr.push(results.data[0]);
+          } else {
+            arr = [];
+          }
+        }
+      }.bind(this)
     });
   }
 
--- src/js/leaflet.textCanvas.js
+++ src/js/leaflet.textCanvas.js
@@ -1,48 +1,61 @@
+import {} from './ctxtextpath'
 /*
  * 线写文字 canvas
  */
 var LineTextCanvas = L.Canvas.extend({
 
-    _updatePoly: function(layer, closed) {
-        L.Canvas.prototype._updatePoly.call(this, layer, closed);
-        if (layer.options.text) {
-            this._text(layer, closed);
-        }
-    },
-
-    /*
-     * 在移除时移除整个canvas，避免遗留多余canvas元素
-     * 只针对气象功能
-     */
-    _removePath: function(layer) {
-        layer._removed = true;
-        L.DomUtil.remove(this._container);
-    },
-
-    _text: function(layer, closed) {
-        var ctx = this._ctx,
-            text = layer.options.text,
-            parts = layer._parts,
-            map = this._map;
-        if (!parts.length) {
-            return;
-        }
-        try {
-            var center = layer.getCenter();
-            var pt = map.latLngToLayerPoint(center);
-            ctx.textAlign = 'start';
-            ctx.textBaseline = 'middle';
-            ctx.font = 'Microsoft YaHei';
-            ctx.fillStyle = "#000";
-            ctx.fillText(text, pt.x, pt.y);
-        } catch (ex) {
-            var width = this._container.width;
-            var height = this._container.height;
-            ctx.clearRect(0, 0, width * 2, height * 2);
-            // console.log(ex);
-            // console.log(this._layers);
-        }
+  _updatePoly: function (layer, closed) {
+    L.Canvas.prototype._updatePoly.call(this, layer, closed);
+    if(layer.options.text) {
+      this._text(layer, closed);
     }
+  },
+
+  /*
+   * 在移除时移除整个canvas，避免遗留多余canvas元素
+   * 只针对气象功能
+   */
+  _removePath: function (layer) {
+    layer._removed = true;
+    L.DomUtil.remove(this._container);
+  },
+
+  _text: function (layer, closed) {
+    var ctx = this._ctx,
+      text = layer.options.text,
+      parts = layer._parts,
+      map = this._map;
+    if(!parts.length) {
+      return;
+    }
+    try {
+      var center = layer.getCenter();
+      var pt = map.latLngToLayerPoint(center);
+      // var txtpaths = [];
+      // for (let i = 0, len = parts.length; i < len; i++){
+      //   txtpaths.push(parts[i].x,parts[i].y);
+      // }
+      ctx.save();
+      ctx.textAlign = 'start';
+      ctx.textBaseline = 'middle';
+      ctx.font = 'normal normal 600 12px normal Microsoft YaHei';
+      ctx.fillStyle = "#000";
+      ctx.strokeStyle = '#fff';
+      ctx.lineWidth = 3;
+      ctx.strokeText(text, pt.x, pt.y);
+      ctx.fillText(text, pt.x, pt.y);
+      // if (txtpaths.length > 6) {
+      //   ctx.textPath(text, txtpaths);        
+      // }
+      ctx.restore();
+    } catch(ex) {
+      var width = this._container.width;
+      var height = this._container.height;
+      ctx.clearRect(0, 0, width * 2, height * 2);
+      // console.log(ex);
+      // console.log(this._layers);
+    }
+  }
 });
 
 /*
@@ -50,36 +63,36 @@ var LineTextCanvas = L.Canvas.extend({
  */
 var RectTextCanvas = L.Canvas.extend({
 
-    initialize: function(options) {
-        L.Canvas.prototype.initialize.call(this, options);
-        // L.Util.setOptions(this,options);
-    },
+  initialize: function (options) {
+    L.Canvas.prototype.initialize.call(this, options);
+    // L.Util.setOptions(this,options);
+  },
 
-    _updatePoly: function(layer, closed) {
-        L.Canvas.prototype._updatePoly.call(this, layer, closed);
-        if (layer.options.text === undefined) {
-            return;
-        } else {
-            this._text(layer, closed);
-        }
-    },
-
-    _text: function(layer, closed) {
-        var ctx = this._ctx,
-            text = layer.options.text,
-            map = this._map;
-        var center = layer.getBounds().getCenter();
-        var pt = map.latLngToLayerPoint(center);
-        ctx.textAlign = 'center';
-        ctx.textBaseline = 'middle';
-        ctx.font = 'Microsoft YaHei';
-        ctx.fillStyle = "#000";
-        ctx.fillText(text, pt.x, pt.y);
+  _updatePoly: function (layer, closed) {
+    L.Canvas.prototype._updatePoly.call(this, layer, closed);
+    if(layer.options.text === undefined) {
+      return;
+    } else {
+      this._text(layer, closed);
     }
+  },
+
+  _text: function (layer, closed) {
+    var ctx = this._ctx,
+      text = layer.options.text,
+      map = this._map;
+    var center = layer.getBounds().getCenter();
+    var pt = map.latLngToLayerPoint(center);
+    ctx.textAlign = 'center';
+    ctx.textBaseline = 'middle';
+    ctx.font = 'Microsoft YaHei';
+    ctx.fillStyle = "#000";
+    ctx.fillText(text, pt.x, pt.y);
+  }
 });
 
 
 export {
-    LineTextCanvas,
-    RectTextCanvas
+  LineTextCanvas,
+  RectTextCanvas
 }
