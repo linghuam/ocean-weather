381823cbeec2995f8623166a6e437da050ab04b2
linghuam <1551827898@qq.com>
2017-11-08 12:23:23 +0800

    visiblity

--- src/js/func.visibility.js
+++ src/js/func.visibility.js
@@ -1,5 +1,6 @@
 import Papa from 'papaparse'
-import { LineTextCanvas } from './leaflet.textCanvas'
+// import { LineTextCanvas } from './leaflet.textCanvas'
+import { VisibilityLayer } from './leaflet.visibility'
 
 export class FuncVisibility {
 
@@ -8,112 +9,42 @@ export class FuncVisibility {
   }
 
   start() {
-    var renderer = this._renderer = new LineTextCanvas();
-    var optionsArea = {
-      renderer: renderer,
-      stroke:false,
-      fill: true,
-      fillColor: '#89FF89'
-    };
-    var optionsLine = {
-      renderer: renderer,
-      color: '#000',
-      weight: 1,
-      fill: false,
-      opacity:0.6
-    };
-    var arr = [];
-
-    // if(this._visibilityFeatureGroup && this._map.hasLayer(this._visibilityFeatureGroup)) {
-    //   this._map.removeLayer(this._visibilityFeatureGroup);
-    // }
-    // this._visibilityFeatureGroup = L.featureGroup([]).addTo(this._map);
-
-    var allSplitLatLngs = [];
-    var allSplitTxt = [];
-
     Papa.parse('./static/data/visibility.csv', {
       download: true,
+      header: false,
       complete: function (results) {
-        // console.log(allSplitLatLngs.length);
-        for (let i =0 , len = allSplitLatLngs.length; i < len; i++){
-                // if(this._map.hasLayer(this._visibilityFeatureGroup)) {
-                  // this._visibilityFeatureGroup.addLayer(L.polygon(allSplitLatLngs[i], optionsArea));
-                  L.polygon(allSplitLatLngs[i], optionsArea).addTo(this._map);
-                // }          
-        }
-        for (let i =0 , len = allSplitLatLngs.length; i < len; i++){
-                // if(this._map.hasLayer(this._visibilityFeatureGroup)) {
-                  optionsLine.text = allSplitTxt[i];
-                  // this._visibilityFeatureGroup.addLayer(L.polyline(allSplitLatLngs[i], optionsLine));
-                  L.polyline(allSplitLatLngs[i], optionsLine).addTo(this._map);
-                // }          
-        }        
-      }.bind(this),
-      step: function (results, parser) {
-        if(results.data[0][0] === '' && arr.length) {
-
-          // handler data -------------start------------------------
-          var latlngs = [];
-          for(var i = 0, len = arr.length; i < len; i++) {
-            var row = arr[i];
-            var lat = +row[0];
-            var lng = +row[1];
-            var value = +row[2];
-            var latlng = L.latLng(lat, lng);
-
-            if(i === 0) {
-              latlngs.push(latlng);
-            } else {
-              var lastlng = (latlngs[latlngs.length - 1]).lng;
-              var extend = Math.abs(lng - lastlng);
-              if(extend >= 180) { //解决经度范围超过180连线异常
-                // if(this._map.hasLayer(this._visibilityFeatureGroup)) {
-                //   this._visibilityFeatureGroup.addLayer(L.polygon(latlngs, optionsArea));
-                //   this._visibilityFeatureGroup.addLayer(L.polyline(latlngs, optionsLine));
-                // }
-                if (latlngs.length > 2) {
-                  allSplitLatLngs.push(latlngs);
-                  allSplitTxt.push(value);
-                }
-                latlngs = [];
-                latlngs.push(latlng);
-              } else {
-                latlngs.push(latlng);
-                if(i === len - 1) {
-                  // if(this._map.hasLayer(this._visibilityFeatureGroup)) {
-                  //   this._visibilityFeatureGroup.addLayer(L.polygon(latlngs, optionsArea));
-                  //   this._visibilityFeatureGroup.addLayer(L.polyline(latlngs, optionsLine));
-                  // }
-                if (latlngs.length > 2) {
-                  allSplitLatLngs.push(latlngs);
-                  allSplitTxt.push(value);                  
-                }                  
-                  latlngs = [];
-                }
-              }
-            }
-          }
-          // handler datq ----------------end-----------------------------
-
-          arr = [];
-        } else {
-          if(arr) {
-            arr.push(results.data[0]);
-          } else {
-            arr = [];
-          }
-        }
+        if (results.errors.length || !results.data.length) return;
+        this.getDataCallback(results.data);
       }.bind(this)
     });
   }
 
   stop　() {
-    // if(this._map.hasLayer(this._visibilityFeatureGroup)) {
-    //   this._map.removeLayer(this._visibilityFeatureGroup);
-    // }
-    if (this._renderer) {
-      this._renderer.remove();
-    }      
+    if (this._map.hasLayer(this._layer)) {
+      this._layer.remove();
+    }
+  }
+
+  getDataCallback (data) {
+    // split data by line
+    var newData = [];
+    var temp = [];
+    var i, len, latlngs, leftlatlngs, rightlatlngs;
+    for (i = 0, len = data.length; i < len; i++){
+        if (data[i].length === 1 || i === len-1) {
+          if (temp.length >= 2) newData.push(temp);
+          temp = [];
+        } else {
+          temp.push(data[i]);
+        }
+    }
+
+    // new layer
+    if (this._map.hasLayer(this._layer)) {
+      this._layer.remove();
+    }
+    this._layer = new VisibilityLayer({}, {
+      data: newData
+    }).addTo(this._map)
   }
 }
