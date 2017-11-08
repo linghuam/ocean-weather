ff6d44f7aa1ba5390b3bc9a31be1bf1b68825931
linghuam <1551827898@qq.com>
2017-11-08 15:52:01 +0800

    flow,flow2

--- src/js/func.flow.js
+++ src/js/func.flow.js
@@ -1,6 +1,5 @@
-import 'leaflet-velocity/dist/leaflet-velocity.min.css'
-import 'leaflet-velocity'
 import Papa from 'papaparse'
+import { FlowLayer } from './ocean.weather.flow'
 
 export class FuncFlow {
 
@@ -12,69 +11,26 @@ export class FuncFlow {
     Papa.parse('./static/data/flow.csv', {
       download: true,
       complete: function (results) {
-        var data = [];
-        var flowUObj = {
-          data: [],
-          header: {
-            "dx": 1.0,
-            "dy": 1.0,
-            "la1": 75,
-            "la2": -75,
-            "lo1": -179,
-            "lo2": 179,
-            "nx": 359,
-            "ny": 151,
-            "parameterCategory": 2,
-            "parameterNumber": 2,
-            "parameterUnit": "m.s-1",
-            "refTime": "2017-02-01 23:00:00"
-          }
+        var datas = results.data;
+        var config = {
+          lat: '0',
+          lng: '1',
+          dir: '3',
+          value: '2',
+          data: datas
         };
-        var flowVObj = {
-          data: [],
-          header: {
-            "dx": 1.0,
-            "dy": 1.0,
-            "la1": 75,
-            "la2": -75,
-            "lo1": -179,
-            "lo2": 179,
-            "nx": 359,
-            "ny": 151,
-            "parameterCategory": 2,
-            "parameterNumber": 3,
-            "parameterUnit": "m.s-1",
-            "refTime": "2017-02-01 23:00:00"
-          }
-        };
-        for(var i = 0, len = results.data.length; i < len; i++) {
-          var value = results.data[i][2] * 1852 / 3600;
-          var rad = Math.PI * results.data[i][3] / 180;
-          flowUObj.data.push(value * Math.sin(rad) * 10);
-          flowVObj.data.push(value * Math.cos(rad) * 10);
+        if(this._map.hasLayer(this._flowLayer)) {
+          this._map.removeLayer(this._flowLayer);
         }
-        data.push(flowUObj, flowVObj);
-        var velocityLayer = this._velocityLayer = L.velocityLayer({
-          displayValues: true,
-          displayOptions: {
-            velocityType: 'Global Flow',
-            displayPosition: 'bottomleft',
-            displayEmptyString: 'No wind data'
-          },
-          data: data,
-          maxVelocity: 15
-        });
-        if(this._map.hasLayer(this._velocityLayer)) {
-          this._map.removeLayer(this._velocityLayer);
-        }
-        this._map.addLayer(velocityLayer);
+        this._flowLayer = new FlowLayer(null, config);
+        this._map.addLayer(this._flowLayer);
       }.bind(this)
     });
   }
 
   stop　() {
-    if(this._map.hasLayer(this._velocityLayer)) {
-      this._map.removeLayer(this._velocityLayer);
+    if(this._map.hasLayer(this._flowLayer)) {
+      this._map.removeLayer(this._flowLayer);
     }
   }
 }
