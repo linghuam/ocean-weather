e598113a88aefc1801155624a64a644450dd8b1d
linghuam <1551827898@qq.com>
2017-11-09 21:37:37 +0800

    wind

--- src/js/func.wind.js
+++ src/js/func.wind.js
@@ -1,4 +1,4 @@
-import Papa from 'papaparse'
+import { ParseData } from './tool.parseData'
 import { WindLayer } from './ocean.weather.wind'
 
 export class FuncWind {
@@ -8,30 +8,32 @@ export class FuncWind {
   }
 
   start() {
-    Papa.parse('./static/data/wind.csv', {
-      download: true,
-      header: false,
-      complete: function (results) {
-        var datas = results.data;
-        var config = {
-          lat: '0',
-          lng: '1',
-          dir: '3',
-          value: '2',
-          data: datas
-        };
-        if(this._map.hasLayer(this._windLayer)) {
-          this._map.removeLayer(this._windLayer);
-        }
-        this._windLayer = new WindLayer(null, config);
-        this._map.addLayer(this._windLayer);
-      }.bind(this)
-    });
+    var url = './static/data/wind.csv';
+    ParseData(url, null ,function (results) {
+      this.getDataCallback(results);
+    }, this);
   }
 
-  stop　() {
-    if(this._map.hasLayer(this._windLayer)) {
-      this._map.removeLayer(this._windLayer);
+  stop() {
+    if(this._map.hasLayer(this._layer)) {
+      this._map.removeLayer(this._layer);
     }
+  }
+
+  getDataCallback (results) {
+    var datas = results.data;
+    datas.shift();
+    var config = {
+      lat: '0',
+      lng: '1',
+      value: '2',
+      dir: '3',
+      data: datas
+    };
+    if(this._map.hasLayer(this._layer)) {
+      this._map.removeLayer(this._layer);
+    }
+    this._layer = new WindLayer({}, config);
+    this._map.addLayer(this._layer);
   }
 }
